77994c1428d553bd87bfef1a19f0b13d4980de21
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-03-24 21:12:49 -0400

    first commit

---
+++ CoffeeShop/Controllers/RegistrationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string fname, string lname, string email, int phone, string password)
+        {
+            ViewData["name"] = fname + " " + lname;
+            ViewData["email"] = email;
+            ViewData["phone"] = phone;
+            ViewData["password"] = password;
+
+            return View();
+        }
+    }
+}
---
+++ CoffeeShop/Controllers/ResultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ResultController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            ViewData["name"] = name;
+
+            return View();
+        }
+    }
+}
